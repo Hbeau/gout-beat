bfe667ec4a7815d5717446475749c84bbaa4088f
hbeau <hugo.beaucamps@gmail.com>
2022-04-11 22:47:58 +0200

    song and costumes

--- src/callbacks/postGameStarted.ts
+++ src/callbacks/postGameStarted.ts
@@ -14,4 +14,10 @@ export function postGameStarted(): void {
   ];
   globals.$step = Steps.OBJECTIVE_SELECTION;
   Isaac.ExecuteCommand("goto s.default.13");
+  Isaac.GetPlayer().AddNullCostume(
+    Isaac.GetCostumeIdByPath("gfx/characters/8265_goutbeat.anm2"),
+  );
+  Isaac.ConsoleOutput(`sound :${Isaac.GetSoundIdByName("Run Victory")}`);
+  SFXManager().Preload(Isaac.GetSoundIdByName("Run Victory"));
+  SFXManager().Play(Isaac.GetSoundIdByName("Run Victory"));
 }
